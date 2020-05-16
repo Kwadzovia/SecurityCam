4eee8be8206a2535767d66ad425305f36a0dbc1c
Selikem Kwadzovia <skwadzov@sfu.ca>
2020-05-15 22:08:17 -0700

    Sleep Wake PIR Test

--- SecurityCam/src/main.cpp
+++ SecurityCam/src/main.cpp
@@ -1,21 +1,52 @@
 #include <Arduino.h>
-const int debugLED = 3;
-const int motionPin = 6;
+#include <avr/sleep.h>
+
+const int debugLED = 6;
+const int motionInterrupt = 3;
 bool initialDelay = false;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(LED_BUILTIN,OUTPUT);
   pinMode(debugLED,OUTPUT);
-  pinMode(motionPin,INPUT);
+  pinMode(motionInterrupt,INPUT);
 }
 
+void wakeUp(){
+  sleep_disable();
+  detachInterrupt(digitalPinToInterrupt(3));
+  Serial.println("BODY DETECTED");
+}
+
+void Go_To_Sleep(){
+  Serial.println("GOING TO SLEEP");
+  sleep_enable();
+  EIFR |= 0x02; //Clear interrupt 1 flag (Datasheet)
+  attachInterrupt(digitalPinToInterrupt(3),wakeUp,HIGH);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(debugLED,LOW);
+  delay(1000);
+  sleep_cpu();
+  Serial.println("SUCCESSFUL WAKEUP");
+  digitalWrite(debugLED,HIGH);
+}
+
+
+
 void loop() {
+  digitalWrite(debugLED,HIGH);
+  delay(10000);
+  Go_To_Sleep();
+
+  delay(10000);
+
   if (initialDelay == false){
     initialDelay = true;
     delay(3000);
   }
   // put your main code here, to run repeatedly:
-  if(digitalRead(motionPin) == HIGH)
+  if(digitalRead(motionInterrupt) == HIGH)
   {
     digitalWrite(debugLED,HIGH);
   }
@@ -24,5 +55,6 @@ void loop() {
     digitalWrite(debugLED,LOW);
   }
   
-  Serial.println(digitalRead(motionPin));
+  Serial.println(digitalRead(motionInterrupt));
 }
+
